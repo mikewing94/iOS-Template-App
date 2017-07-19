44b14ce820c13ec04552910f6b0332967f9565e5
Michael Wing <mikewing@michaels-macbook-air.local>
2017-07-19 09:29:30 +0100

    Initial Commit

---
+++ universallayout/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  universallayout
+//
+//  Created by Michael Wing on 19/07/2017.
+//  Copyright © 2017 Michael Wing. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
