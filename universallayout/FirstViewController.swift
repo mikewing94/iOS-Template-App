e1a0302770cce65b42c429e556a71a0d50217734
Mike Wing <michael.wing@rocketmail.com>
2017-07-19 12:09:12 +0100

    Developing a Universal Layout for iOS Apps
    Example xCode Project after finding there wasn't many templates to begin developing from.
    
    Based on the tabbed App example by Apple.
    
    Features WebView and Access to users location (GPS)

--- universallayout/FirstViewController.swift
+++ universallayout/FirstViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var webview: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let NSURL = URL(string: "https://bigbaleapp.resolutiontelevision.com")
+        let NSURLRequest = URLRequest(url: NSURL!)
+        webview.loadRequest(NSURLRequest)
     }
 
     override func didReceiveMemoryWarning() {
