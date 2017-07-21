7367f0dc1e3e448e21bd60fc1e4f4863bdafc625
Mike Wing <michael.wing@rocketmail.com>
2017-07-21 11:29:56 +0100

    Added in Reachability
    Added internet detection, if there is no internet then a message is printed to the console and a "No Wifi Image" is displayed to the end-user

--- universallayout/FirstViewController.swift
+++ universallayout/FirstViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class FirstViewController: UIViewController {
+    
+    @IBOutlet weak var myNoWifi: UIImageView!
 
     @IBOutlet weak var webview: UIWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        
+        if currentReachabilityStatus == .notReachable {
+            print("There is no Internet Connection")
+           myNoWifi.isHidden = false
+        }
+
+        
         let NSURL = URL(string: "https://bigbaleapp.resolutiontelevision.com")
         let NSURLRequest = URLRequest(url: NSURL!)
         webview.loadRequest(NSURLRequest)
